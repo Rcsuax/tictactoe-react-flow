04bd6982d101f860ff8440fab2dec75f865910a7
rcsuax <rcsuax@gmail.com>
2017-05-17 16:22:41 +0100

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-echo -e "public/ \nsrc/ \n.babelrc \n.flowconfig \npackage.json" >> .gitignore
+echo -e "public/ \nsrc/ \n.babelrc \n.flowconfig \npackage.json \nREADME.md" >> .gitignore
 rsync -a build/* .
